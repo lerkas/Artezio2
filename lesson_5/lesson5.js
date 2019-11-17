4c72f24a0740b098aef09d08b172a3426a98b094
lerkas <lerkas@gmail.com>
2019-11-17 23:06:44 +0300

    GET and POST request

--- lesson_5/lesson5.js
+++ lesson_5/lesson5.js
@@ -9,52 +9,51 @@
 
 (function() {
 
-    // GET - запрос
-    const url = 'https://reqres.in/api/users/2';
-
-    async function getData(url) {
-        const response = await fetch(url);
-        const json = await response.json();
-        console.log(JSON.stringify(json)); // Данные в виде строки
-        console.log(json); 				   // Данные в виде объекта
-
-    }
-
-    getData(url);
-
-    // GET - запрос с обработкой ошибок
-    try {
-        const response = await fetch(url);
-        const contentType = response.headers.get('content-type');
-        if (!contentType || !contentType.includes('application/json')) {
-            throw new TypeError('Ошибка: JSON не получен');
-        }
-        const json = await response.json();
-        console.log(JSON.stringify(json)); // Данные в виде строки
-        console.log(json); 				   // Данные в виде объекта
-    } catch (error) {
-        console.log(error);
-    }
-
-    // POST - запрос
     const url = 'https://reqres.in/api/users/2';
     const data = {
         'name': 'morpheus',
         'job': 'leader'
     }
 
-    try {
-        const response = await fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
+    // GET - запрос с обработкой ошибок
+    async function getData(url) {
+        try {
+            const response = await fetch(url);
+            const contentType = response.headers.get('content-type');
+            if (!contentType || !contentType.includes('application/json')) {
+                throw new TypeError('Ошибка: JSON не получен');
             }
-        });
-        const json = await response.json();
-        console.log('Успех:', JSON.stringify(json));
-    } catch (error) {
-        console.error('Ошибка:', error);
+            const json = await response.json();
+            console.log('Успех:', JSON.stringify(json)); // Данные в виде строки
+            console.log('Успех:', json); // Данные в виде объекта
+        } catch (error) {
+            console.log('Ошибка:', error);
+        }
     }
+
+    // POST - запрос c обработкой ошибок
+    async function postData(url, data) {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const contentType = response.headers.get('content-type');
+            if (!contentType || !contentType.includes('application/json')) {
+                throw new TypeError('Ошибка: JSON не получен');
+            }
+            const json = await response.json();
+            console.log('Успех:', JSON.stringify(json));
+            console.log('Успех:', json);
+        } catch (error) {
+            console.log('Ошибка:', error);
+        }
+    }
+
+    getData(url);
+    postData(url, data);
 
 }());
